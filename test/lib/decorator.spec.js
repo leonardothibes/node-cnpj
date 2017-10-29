21455e31dbc8f095ba376f8489f6e9da3ffa5f98
Leonardo Thibes <leonardothibes@gmail.com>
2017-10-29 20:20:38 -0200

    wip

--- test/lib/decorator.spec.js
+++ test/lib/decorator.spec.js
@@ -1,3 +1,12 @@
 'use strict';
 
-const decorator = require('../../lib/decorator');
+const decorator = require('../../lib/decorator'),
+      assert    = require('unit.js');
+
+describe('Decorator', function()
+{
+    it('Is Masked', function(done)
+    {
+        done();
+    });
+});
